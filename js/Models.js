2d7b47806f05b45074cfc01ebf645ee923d48e72
unknown <jggianakopoulos@gmail.com>
2020-04-12 15:47:33 -0400

    Adding new features

--- js/Models.js
+++ js/Models.js
@@ -15,7 +15,7 @@ var AvailableClassView = Backbone.View.extend ({
 	"askAdd" : function () {
 		console.log("askadd");
 		days = this.model.get("days");
-		add = true;
+		popup = true;
 		for(var i = 0; i < window.ClassCollection.length; i++) {
 			overlap = false;
 			var occupied_days = window.ClassCollection.at(i).get("days").split(";");
@@ -30,16 +30,20 @@ var AvailableClassView = Backbone.View.extend ({
 				e_start_time = window.ClassCollection.at(i).get("start_time");
 				e_end_time = window.ClassCollection.at(i).get("end_time");
 				if (this.model.get("start_time") < e_end_time && this.model.get("end_time") > e_start_time) {
-					add = false;
+					popup = false;
 					break;
 				}
 			}
 		}
 
-		if (add) {
-			window.ClassCollection.add(this.model);
+		if (popup) {
+			window.current_model = this.model;
+			$(".add-popup").removeClass("invisible");
 		} else {
-			console.log("cant add");
+			$(".error-popup").removeClass("invisible");
+			setTimeout(function() {
+				$(".error-popup").addClass("invisible"); 
+			}, 1500);
 		}
 	}
 });
@@ -50,10 +54,9 @@ var AvailableClassViewHolder = Backbone.View.extend({
 		this.$el.html(this.template());
 		$collectionHolder = document.createElement("div");
 		_.each(window.AvailableClassCollection.models, function(model) {
-			console.log("av");
 			var avclass = new AvailableClassView({
 				model: model
-			})
+			});
 
 			$collectionHolder.append(avclass.render().el);
 		}.bind(this));
@@ -94,6 +97,27 @@ var AvailableClassViewHolder = Backbone.View.extend({
 	}
 });
 
+
+var ScheduledClassView = Backbone.View.extend ({
+	template: _.template($("#scheduledclassview").html()),
+	class:"AvailableClassView",
+	render: function () {
+		this.$el.html(this.template(this.model.attributes));
+		return this;
+	},
+	initialize : function() {
+		this.render();
+	},
+	"events" : {
+		"click" : "askRemove"
+	},
+	"askRemove" : function () {
+		window.remove_model = this.model;
+		$(".remove-popup").removeClass("invisible");
+	}
+});
+
+
 ClassModel = Backbone.Model.extend ({
 	initialize : function () {
 	},
@@ -101,7 +125,8 @@ ClassModel = Backbone.Model.extend ({
 		days : "monday;wednesday",
 		start_time: "3:00",
 		end_time: "5:00"
-	}
+	},
+	comparator: 'start_time'
 
 });
 
@@ -121,12 +146,53 @@ VisibleAvailableClassCollection = Backbone.Collection.extend({
 	model: ClassModel
 });
 
+MondayClassCollection = Backbone.Collection.extend({
+	model: ClassModel
+});
+
+TuesdayClassCollection = Backbone.Collection.extend({
+	model: ClassModel
+});
+WednesdayClassCollection = Backbone.Collection.extend({
+	model: ClassModel
+});
+ThursdayClassCollection = Backbone.Collection.extend({
+	model: ClassModel
+});
+FridayClassCollection = Backbone.Collection.extend({
+	model: ClassModel
+});
 window.ClassCollection = new ClassCollection();
 window.AvailableClassCollection = new AvailableClassCollection();
+window.MondayClassCollection = new MondayClassCollection();
+window.TuesdayClassCollection = new TuesdayClassCollection();
+window.WednesdayClassCollection = new WednesdayClassCollection();
+window.ThursdayClassCollection = new ThursdayClassCollection();
+window.FridayClassCollection = new FridayClassCollection();
+
 
 window.AvailableClassCollection.add({name:"Class 1",start_time:"17:00",end_time:"19:00",days:"monday", subject:"APM",course_number:"1500"});
 window.AvailableClassCollection.add({name:"Class 2",start_time:"16:00",end_time:"19:00",days:"tuesday",subject:"PHY",course_number:"2400"});
 window.AvailableClassCollection.add({name:"Class 3",start_time:"15:00",end_time:"16:00",days:"monday",subject:"IS",course_number:"1600"});
 window.AvailableClassCollection.add({name:"Class 4",start_time:"09:00",end_time:"11:47",days:"friday",subject:"CS",course_number:"3500"});
+window.AvailableClassCollection.add({name:"Class 1",start_time:"17:00",end_time:"19:00",days:"monday", subject:"APM",course_number:"1500"});
+window.AvailableClassCollection.add({name:"Class 2",start_time:"16:00",end_time:"19:00",days:"tuesday",subject:"PHY",course_number:"2400"});
+window.AvailableClassCollection.add({name:"Class 3",start_time:"15:00",end_time:"16:00",days:"monday",subject:"IS",course_number:"1600"});
+window.AvailableClassCollection.add({name:"Class 4",start_time:"09:00",end_time:"11:47",days:"friday",subject:"CS",course_number:"3500"});
+window.AvailableClassCollection.add({name:"Class 1",start_time:"17:00",end_time:"19:00",days:"monday", subject:"APM",course_number:"1500"});
+window.AvailableClassCollection.add({name:"Class 2",start_time:"16:00",end_time:"19:00",days:"tuesday",subject:"PHY",course_number:"2400"});
+window.AvailableClassCollection.add({name:"Class 3",start_time:"15:00",end_time:"16:00",days:"monday",subject:"IS",course_number:"1600"});
+window.AvailableClassCollection.add({name:"Class 4",start_time:"09:00",end_time:"11:47",days:"friday",subject:"CS",course_number:"3500"});
+window.AvailableClassCollection.add({name:"Class 1",start_time:"17:00",end_time:"19:00",days:"monday", subject:"APM",course_number:"1500"});
+window.AvailableClassCollection.add({name:"Class 2",start_time:"16:00",end_time:"19:00",days:"tuesday",subject:"PHY",course_number:"2400"});
+window.AvailableClassCollection.add({name:"Class 3",start_time:"15:00",end_time:"16:00",days:"monday",subject:"IS",course_number:"1600"});
+window.AvailableClassCollection.add({name:"Class 4",start_time:"09:00",end_time:"11:47",days:"friday",subject:"CS",course_number:"3500"});
+window.AvailableClassCollection.add({name:"Class 1",start_time:"17:00",end_time:"19:00",days:"monday", subject:"APM",course_number:"1500"});
+window.AvailableClassCollection.add({name:"Class 2",start_time:"16:00",end_time:"19:00",days:"tuesday",subject:"PHY",course_number:"2400"});
+window.AvailableClassCollection.add({name:"Class 3",start_time:"15:00",end_time:"16:00",days:"monday",subject:"IS",course_number:"1600"});
+window.AvailableClassCollection.add({name:"Class 4",start_time:"09:00",end_time:"11:47",days:"monday;friday",subject:"CS",course_number:"3500"});
+
+
+
 
 window.VisibleAvailableClassCollection = window.AvailableClassCollection;
